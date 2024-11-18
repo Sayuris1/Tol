6a765378af38eaad315bcc5804cb1c149806b22a
Alp <elgunalp1@gmail.com>
2024-11-18 03:35:28 +0000

    Scoreboard working.

--- prefabs/weapon.go
+++ prefabs/weapon.go
@@ -13,17 +13,6 @@ components {
   }
 }
 embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "default_animation: \"anim\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/builtins/graphics/particle_blob.tilesource\"\n"
-  "}\n"
-  ""
-}
-embedded_components {
   id: "bullet_factory"
   type: "factory"
   data: "prototype: \"/prefabs/bullet.go\"\n"
